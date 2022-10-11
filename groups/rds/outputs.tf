e8af10da38b5f84bcf52d07fbcb42e24a2b82628
autmwatl <noreply@companieshouse.gov.uk>
2022-10-11 21:50:04 +0100

    remove idea file

--- groups/rds/outputs.tf
+++ groups/rds/outputs.tf
@@ -1,7 +1,7 @@
 output vpc_ids {
-  value = var.vpc_ids
+  value = vpc_ids
 }
 
 output subnet_ids {
-  value = var.subnet_ids
+  value = subnet_ids
 }
--- groups/rds/variables.tf
+++ groups/rds/variables.tf
@@ -20,29 +20,6 @@ variable "default_instance_type" {
   type        = string
 }
 
-variable "instance_specifications" {
-  description = "A map of specifications for the instances"
-  type = map(map(map(string)))
-}
-
-variable "lvm_block_definitions" {
-  default = [
-    {
-      aws_volume_size_gb: "50",
-      filesystem_resize_tool: "xfs_growfs",
-      lvm_logical_volume_device_node: "/dev/kafka/data",
-      lvm_physical_volume_device_node: "/dev/xvdb"
-    }
-  ]
-  description = "Kafka LVM block definitions"
-  type = list(object({
-    aws_volume_size_gb: string,
-    filesystem_resize_tool: string,
-    lvm_logical_volume_device_node: string,
-    lvm_physical_volume_device_node: string,
-  }))
-}
-
 variable "region" {
   description = "The AWS region in which resources will be created"
   type        = string
