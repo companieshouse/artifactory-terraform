ed54dc0a057bb5162ebf7724ce833e760aa7eb6e
autmwatl <noreply@companieshouse.gov.uk>
2022-10-27 14:50:05 +0100

    DVOP-2219 remove unnecessary blocks

--- groups/rds/vpc.tf
+++ groups/rds/vpc.tf
@@ -1,27 +1,5 @@
 data "aws_region" "current" {}
 
-resource "aws_vpc_ipam" "main" {
-  operating_regions {
-    region_name = data.aws_region.current.name
-  }
-}
-
-resource "aws_vpc_ipam_pool" "main" {
-  address_family = "ipv4"
-  ipam_scope_id  = aws_vpc_ipam.main.private_default_scope_id
-  locale         = data.aws_region.current.name
-}
-
-data "aws_vpc" "all_vpc_id" {
-  for_each = data.aws_vpc.all_vpc_id[*].id
-  id = each.value
-}
-
-data "aws_subnet" "placement" {
-  for_each = data.aws_subnet_ids.placement.ids
-  id = each.value
-}
-
 data "aws_subnet_ids" "placement" {
   vpc_id = data.aws_vpc.placement.id
 
@@ -29,4 +7,9 @@ data "aws_subnet_ids" "placement" {
     name   = "tag:Name"
     values = [local.placement_subnet_pattern]
   }
+}
+
+data "aws_subnet" "placement" {
+  for_each = data.aws_subnet_ids.placement.ids
+  id = each.value
 }
