c0d5194ff9a7f747f9a311be09a38f483c7c7e8a
autmwatl <noreply@companieshouse.gov.uk>
2023-04-06 15:52:17 +0100

    remove system.yaml from cloud init to test setup

--- groups/instance/cloud_init.tf
+++ groups/instance/cloud_init.tf
@@ -7,15 +7,15 @@ data "cloudinit_config" "artifactory" {
     content      = "#cloud-config"
   }
 
-  part {
-    content_type = "text/cloud-config"
-    content = templatefile("${path.module}/cloud-init/templates/system.yml.tpl", {
-      db_fqdn     = local.db_fqdn
-      db_username = local.db_username
-      db_password = local.db_password
-
-    })
-  }
+#  part {
+#    content_type = "text/cloud-config"
+#    content = templatefile("${path.module}/cloud-init/templates/system.yml.tpl", {
+#      db_fqdn     = local.db_fqdn
+#      db_username = local.db_username
+#      db_password = local.db_password
+#
+#    })
+#  }
 
   part {
     content_type   = "text/cloud-config"
