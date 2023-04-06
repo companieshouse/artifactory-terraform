a57284c6294347e0b399398571e560dcf7f0baa3
autmwatl <noreply@companieshouse.gov.uk>
2023-04-06 15:53:25 +0100

    remove unnecessary vars

--- groups/artifactory/locals.tf
+++ groups/artifactory/locals.tf
@@ -1,27 +1,28 @@
 locals {
-  secrets     = data.vault_generic_secret.secrets.data
-  artifactory_access_token                                 = local.secrets.artifactory_access_token
-  server_url = "${var.service}.${var.environment}.${data.aws_route53_zone.selected.name}:8081/artifactory"
+  secrets                                = data.vault_generic_secret.secrets.data
+  artifactory_access_token               = local.secrets.artifactory_access_token
+  server_url                             = "${var.service}.${var.environment}.${data.aws_route53_zone.selected.name}:8081/artifactory"
+  dns_zone_name                          = local.secrets.dns_zone_name
 
-  ldapSetting_id                                           = "ldap1"
-  ldapSetting_emailAttribute                               = local.secrets.ldapSetting_emailAttribute
-  ldapSetting_ldapUrl                                      = local.secrets.ldapSetting_ldapUrl
-  ldapSetting_managerDn                                    = local.secrets.ldapSetting_managerDn
-  ldapSetting_managerPassword                              = local.secrets.ldapSetting_managerPassword
-  ldapSetting_searchBase                                   = local.secrets.ldapSetting_searchBase
-  ldapSetting_searchFilter                                 = local.secrets.ldapSetting_searchFilter
-  ldapSetting_searchSubTree                                = local.secrets.ldapSetting_searchSubTree
-  ldapSetting_userDnPattern                                = local.secrets.ldapSetting_userDnPattern
-  ldapSetting_allowUserToAccessProfile                     = local.secrets.ldapSetting_allowUserToAccessProfile
-  ldapSetting_descriptionAttribute                         = local.secrets.ldapSetting_descriptionAttribute
-  ldapGroupSettings_descriptionAttribute                   = local.secrets.ldapGroupSettings_descriptionAttribute
-  ldapGroupSettings_enabledLdap                            = local.ldapSetting_id
-  ldapGroupSettings_filter                                 = local.secrets.ldapGroupSettings_filter
-  ldapGroupSettings_groupBaseDn                            = local.secrets.ldapGroupSettings_groupBaseDn
-  ldapGroupSettings_groupMemberAttribute                   = local.secrets.ldapGroupSettings_groupMemberAttribute
-  ldapGroupSettings_groupNameAttribute                     = local.secrets.ldapGroupSettings_groupNameAttribute
-  ldapGroupSettings_strategy                               = local.secrets.ldapGroupSettings_strategy
-  ldapGroupSettings_subTree                                = local.secrets.ldapGroupSettings_subTree
+  ldapSetting_id                         = "ldap1"
+  ldapSetting_emailAttribute             = local.secrets.ldapSetting_emailAttribute
+  ldapSetting_ldapUrl                    = local.secrets.ldapSetting_ldapUrl
+  ldapSetting_managerDn                  = local.secrets.ldapSetting_managerDn
+  ldapSetting_managerPassword            = local.secrets.ldapSetting_managerPassword
+  ldapSetting_searchBase                 = local.secrets.ldapSetting_searchBase
+  ldapSetting_searchFilter               = local.secrets.ldapSetting_searchFilter
+  ldapSetting_searchSubTree              = local.secrets.ldapSetting_searchSubTree
+  ldapSetting_userDnPattern              = local.secrets.ldapSetting_userDnPattern
+  ldapSetting_allowUserToAccessProfile   = local.secrets.ldapSetting_allowUserToAccessProfile
+  ldapSetting_descriptionAttribute       = local.secrets.ldapSetting_descriptionAttribute
+  ldapGroupSettings_descriptionAttribute = local.secrets.ldapGroupSettings_descriptionAttribute
+  ldapGroupSettings_enabledLdap          = local.ldapSetting_id
+  ldapGroupSettings_filter               = local.secrets.ldapGroupSettings_filter
+  ldapGroupSettings_groupBaseDn          = local.secrets.ldapGroupSettings_groupBaseDn
+  ldapGroupSettings_groupMemberAttribute = local.secrets.ldapGroupSettings_groupMemberAttribute
+  ldapGroupSettings_groupNameAttribute   = local.secrets.ldapGroupSettings_groupNameAttribute
+  ldapGroupSettings_strategy             = local.secrets.ldapGroupSettings_strategy
+  ldapGroupSettings_subTree              = local.secrets.ldapGroupSettings_subTree
 
 
 }
--- groups/artifactory/variables.tf
+++ groups/artifactory/variables.tf
@@ -8,18 +8,6 @@ variable "environment" {
   type        = string
 }
 
-variable "default_ami_version_pattern" {
-  description = "The default AMI version pattern to use when matching AMIs for instances"
-  default =   "\\d.\\d.\\d-\\d+"
-  type        = string
-}
-
-variable "default_instance_type" {
-  description = "The default instance type to use for instances"
-  default     = "t3.medium"
-  type        = string
-}
-
 variable "region" {
   description = "The AWS region in which resources will be created"
   type        = string
@@ -43,80 +31,3 @@ variable "team" {
   type        = string
 }
 
-variable "db_port" {
-  description = "The port that the database can be reached on"
-  default     = 5432
-  type        = number
-
-}
-variable "db_engine" {
-  description = "Database engine"
-  default     = "postgres"
-  type        = string
-}
-
-variable "db_engine_major_version" {
-  description = "Database engine major version"
-  default     = "13"
-  type        = string
-}
-
-variable "db_engine_minor_version" {
-  description = "Database engine minor version"
-  default     = "9"
-  type        = string
-}
-
-variable "db_instance_class" {
-  description = "Database instance class"
-  default     = "db.t4g.small"
-  type        = string
-}
-
-variable "db_storage_type" {
-  description = "Database storage type"
-  default     = "gp2"
-  type        = string
-}
-
-variable "db_storage_gb" {
-  description = "Database storage gigabytes"
-  default     = 20
-  type        = number
-}
-
-variable "rds_cloudwatch_export_logs_retention_period" {
-  description = "The set value for retention period of rds export cloudwatch logs, setting the period in days value"
-  default     = 180
-  type        = number
-}
-
-variable "rds_cloudwatch_logs_exports" {
-  description = "List of chosen log exports for database RDS Cloudwatch Logs"
-  default     = ["postgresql", "upgrade"]
-  type        = list(any)
-}
-
-variable "db_deletion_protection" {
-  description = "Database deletion protection"
-  default     = true
-  type        = bool
-}
-
-variable "db_backup_retention_period" {
-  description = "Database backup retention period"
-  default     = 7
-  type        = number
-}
-
-variable "db_backup_window" {
-  description = "Database backup window"
-  default     = "03:00-06:00"
-  type        = string
-}
-
-variable "db_maintenance_window" {
-  description = "Database maintenance window"
-  default     = "Sat:00:00-Sat:03:00"
-  type        = string
-}
