e324b7b43ac5c46f05dd4d7f789eba0b7c1b9c52
autmwatl <noreply@companieshouse.gov.uk>
2023-04-06 15:53:38 +0100

    correct var name

--- groups/artifactory/providers.tf
+++ groups/artifactory/providers.tf
@@ -1,5 +1,5 @@
 provider "artifactory" {
-  url          = local.server_url
-  access_token = local.access_token
+  url           = local.server_url
+  access_token  = local.artifactory_access_token
   check_license = false  # TODO Should be true when license is available
 }
