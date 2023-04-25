2c1a7ae86e36002566e89d55df7015ee943462b2
lee-mk-joseph <ljoseph@companieshouse.gov.uk>
2023-04-25 15:12:58 +0100

    push new deployment

--- groups/instance/cloud_init.tf
+++ groups/instance/cloud_init.tf
@@ -37,7 +37,7 @@ data "cloudinit_config" "artifactory" {
       http_proxy_port                        = ""
       artifactory_access_token               = local.artifactory_access_token
       db_fqdn                                = local.db_fqdn
-      #db_name                                = var.service
+      db_name                                = var.service
       db_username                            = local.db_username
       db_password                            = local.db_password
     })
