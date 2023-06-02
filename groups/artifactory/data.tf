12af355de15bffbd09fc87ac46d8cf833d0051bd
lee-mk-joseph <ljoseph@companieshouse.gov.uk>
2023-06-02 10:40:19 +0100

    removal of unused vault_generic_secret_account_ids

--- groups/artifactory/data.tf
+++ groups/artifactory/data.tf
@@ -2,10 +2,6 @@ data "aws_ec2_managed_prefix_list" "administration" {
   name         = "administration-cidr-ranges"
 }
 
-data "vault_generic_secret" "account_ids" {
-  path         = "aws-accounts/account-ids"
-}
-
 data "vault_generic_secret" "secrets" {
   path         = "team-${var.team}/${var.account_name}/${var.region}/${var.environment}/${var.repository_name}"
 }
