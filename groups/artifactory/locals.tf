fb8903f7409e3d1f8437d469b9efa4edbfd19e98
lee-mk-joseph <ljoseph@companieshouse.gov.uk>
2023-06-05 16:14:20 +0100

    reverting server_url changes for testing

--- groups/artifactory/locals.tf
+++ groups/artifactory/locals.tf
@@ -1,5 +1,6 @@
 locals {
   secrets                                     = data.vault_generic_secret.secrets.data
   artifactory_access_token                    = local.secrets.artifactory_access_token
-  server_url                                  = "http://${var.service}.${var.environment}.${local.secrets.dns_zone_name}:8081/artifactory"
+  server_url                                  = "${var.service}.${var.environment}.${data.aws_route53_zone.selected.name}:8081/artifactory"
+  #server_url                                  = "http://${var.service}.${var.environment}.${local.secrets.dns_zone_name}:8081/artifactory"
 }
