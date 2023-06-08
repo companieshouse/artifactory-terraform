cf5781aba0bf4feea7215e445bdcfe8e72f42410
lee-mk-joseph <ljoseph@companieshouse.gov.uk>
2023-06-08 11:54:17 +0100

    Provider removal form both groups

--- groups/instance/providers.tf
+++ groups/instance/providers.tf
@@ -9,8 +9,8 @@ provider "aws" {
   }
 }
 
-provider "artifactory" {
-  url           = local.server_url
-  access_token  = local.artifactory_access_token
-  check_license = false  # TODO Should be true when license is available
-}
+#provider "artifactory" {
+#  url           = local.server_url
+#  access_token  = local.artifactory_access_token
+#  check_license = false  # TODO Should be true when license is available
+#}
