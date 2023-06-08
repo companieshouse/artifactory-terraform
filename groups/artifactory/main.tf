0c51530df6eb2d586b67fcffa879bf0fb59f3a88
lee-mk-joseph <ljoseph@companieshouse.gov.uk>
2023-06-08 12:21:43 +0100

    removed art group provider

--- groups/artifactory/main.tf
+++ groups/artifactory/main.tf
@@ -1,12 +1,12 @@
 terraform {
   required_version = ">= 0.13.0, < 0.14"
 
-  required_providers {
-    artifactory = {
-      source = "jfrog/artifactory"
-      version = "7.4.3"
-    }
-  }
+  #required_providers {
+    #artifactory = {
+      #source = "jfrog/artifactory"
+      #version = "7.4.3"
+    #}
+  #}
 
   backend "s3" {}
 }
