ff537d3b543c59ff9c362273255749140334fd00
lee-mk-joseph <ljoseph@companieshouse.gov.uk>
2023-06-08 12:49:51 +0100

    add provider back in

--- groups/instance/providers.tf
+++ groups/instance/providers.tf
@@ -11,9 +11,7 @@ provider "aws" {
 
 provider "artifactory" {
   url           = local.server_url
-  #access_token  = local.artifactory_access_token
+  access_token  = local.artifactory_access_token
   #access_token  = ";"
-  username      = "admin"
-  password      = "Password1!"
   check_license = false  # TODO Should be true when license is available
 }
