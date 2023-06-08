34552c39b7a1c4c5a45916081ac24dbbde45f335
lee-mk-joseph <ljoseph@companieshouse.gov.uk>
2023-06-08 12:30:24 +0100

    adding providers back in

--- groups/instance/providers.tf
+++ groups/instance/providers.tf
@@ -9,9 +9,9 @@ provider "aws" {
   }
 }
 
-#provider "artifactory" {
-#  url           = local.server_url
-  #access_token  = local.artifactory_access_token
-#  access_token  = ";"
-#  check_license = false  # TODO Should be true when license is available
-#}
+provider "artifactory" {
+  url           = local.server_url
+  access_token  = local.artifactory_access_token
+  #access_token  = ";"
+  check_license = false  # TODO Should be true when license is available
+}
