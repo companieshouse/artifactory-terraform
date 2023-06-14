6696128e674eb07491323d58f778a05b453971a4
lee-mk-joseph <ljoseph@companieshouse.gov.uk>
2023-06-14 13:06:10 +0100

    removing provider

--- groups/instance/locals.tf
+++ groups/instance/locals.tf
@@ -4,7 +4,7 @@ locals {
 
   secrets                                                  = data.vault_generic_secret.secrets.data
   #server_url                                               = "http://${var.service}.${var.environment}.${local.secrets.dns_zone_name}:8081/artifactory"
-  server_url                                               = "${var.service}.${var.environment}.${data.aws_route53_zone.selected.name}:8081/artifactory"
+  server_url                                               = "http://${var.service}.${var.environment}.${data.aws_route53_zone.selected.name}:8081/artifactory"
   placement_subnet_cidrs                                   = values(zipmap(
     values(data.aws_subnet.placement).*.availability_zone,
     values(data.aws_subnet.placement).*.cidr_block
--- groups/instance/main.tf
+++ groups/instance/main.tf
@@ -6,12 +6,12 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 3.0, < 4.0"
     }
-    artifactory = {
-      source = "jfrog/artifactory"
-      #source  = "registry.terraform.io/jfrog/artifactory"
-      version = "7.4.3"
-    }
+    #artifactory = {
+    #  source = "jfrog/artifactory"
+    #  #source  = "registry.terraform.io/jfrog/artifactory"
+    #  version = "7.4.3"
+    #}
   }
 
-  #backend "s3" {}
+  backend "s3" {}
 }
