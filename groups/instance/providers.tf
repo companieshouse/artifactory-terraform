fe414df0e21f2bdf99a78954d71d311faa64979f
lee-mk-joseph <ljoseph@companieshouse.gov.uk>
2023-06-16 13:05:56 +0100

    Removing artifactory LDAP resources and providers

--- groups/instance/providers.tf
+++ groups/instance/providers.tf
@@ -8,10 +8,3 @@ provider "aws" {
     }
   }
 }
-
-#provider "artifactory" {
-#  url          = local.server_url
-#  access_token = local.artifactory_access_token
-#  #access_token  = ";"
-#  check_license = false # TODO Should be true when license is available
-#}
