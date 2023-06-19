1a281fd421f3cf3bff9dd0a4c71127d5ff51d242
autmwatl <noreply@companieshouse.gov.uk>
2023-06-19 15:52:50 +0100

    DVOP-2349 force HTTPS

--- groups/instance/lb.tf
+++ groups/instance/lb.tf
@@ -72,6 +72,34 @@ resource "aws_lb_listener" "listener_8082" {
   }
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.artifactory.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.artifactory.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = local.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front_end_8081.arn
+  }
+}
+
 
 resource "aws_lb_target_group_attachment" "attach_8081" {
   target_group_arn = aws_lb_target_group.front_end_8081.arn
--- groups/instance/security_groups.tf
+++ groups/instance/security_groups.tf
@@ -42,9 +42,17 @@ resource "aws_security_group" "alb_security_group" {
   vpc_id      = data.aws_vpc.placement.id
 
   ingress {
-    description      = "lb ingress from admin and concourse CIDRs"
+    description      = "lb HTTP ingress from admin and concourse CIDRs"
     from_port        = 80
     to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = local.artifactory_web_access
+  }
+
+  ingress {
+    description      = "lb HTTPS ingress from admin and concourse CIDRs"
+    from_port        = 443
+    to_port          = 443
     protocol         = "tcp"
     cidr_blocks      = local.artifactory_web_access
   }
