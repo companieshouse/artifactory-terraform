3f33254e56d06df32ebff5124ba9e1b620cb67dd
autmwatl <noreply@companieshouse.gov.uk>
2023-06-23 10:30:46 +0100

    DVOP-2349 requested changes

--- groups/instance/lb.tf
+++ groups/instance/lb.tf
@@ -12,25 +12,6 @@ resource "aws_lb" "artifactory" {
   }
 }
 
-resource "aws_lb_target_group" "front_end_8081" {
-  name     = "${var.environment}-${var.service}-tg-8081"
-  port     = 8081
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.placement.id
-
-  health_check {
-    enabled             = true
-    interval            = 30
-    path                = "/"
-    port                = "8081"
-    timeout             = 3
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-    protocol            = "HTTP"
-    matcher             = "200"
-  }
-}
-
 resource "aws_lb_target_group" "front_end_8082" {
   name     = "${var.environment}-${var.service}-tg-8082"
   port     = 8082
@@ -47,17 +28,6 @@ resource "aws_lb_target_group" "front_end_8082" {
     unhealthy_threshold = 3
     protocol            = "HTTP"
     matcher             = "200"
-  }
-}
-
-resource "aws_lb_listener" "listener_8082" {
-  load_balancer_arn = aws_lb.artifactory.arn
-  port              = "8082"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.front_end_8082.arn
   }
 }
 
@@ -87,13 +57,6 @@ resource "aws_lb_listener" "https" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.front_end_8082.arn
   }
-}
-
-
-resource "aws_lb_target_group_attachment" "attach_8081" {
-  target_group_arn = aws_lb_target_group.front_end_8081.arn
-  target_id        = aws_instance.artifactory.arn
-  port             = 8081
 }
 
 resource "aws_lb_target_group_attachment" "attach_8082" {
