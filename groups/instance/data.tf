bd670e78cf4583043388fd4adbe5e4d0f8282407
lee-mk-joseph <ljoseph@companieshouse.gov.uk>
2023-11-08 12:20:54 +0000

    Enable EFS Encryption

--- groups/instance/data.tf
+++ groups/instance/data.tf
@@ -17,6 +17,10 @@ data "vault_generic_secret" "security_s3_buckets" {
   path = "aws-accounts/security/s3"
 }
 
+data "vault_generic_secret" "security_efs_kms_keys_data" {
+  path = "aws-accounts/shared-services/kms"
+}
+
 data "aws_caller_identity" "current" {}
 
 data "aws_vpc" "placement" {
