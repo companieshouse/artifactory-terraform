6463e7704efecc9571b45dd3856770052a974a7e
lee-mk-joseph <ljoseph@companieshouse.gov.uk>
2024-02-21 18:15:19 +0000

    Implement ASG

--- groups/instance/asg.tf
+++ groups/instance/asg.tf
@@ -1,72 +1,86 @@
-# resource "aws_autoscaling_group" "artifactory_asg" {
-#   name                      = "${var.environment}-${var.service}-asg"  
-#   availability_zones        = [var.region]
-#   desired_capacity          = 1
-#   max_size                  = 1
-#   min_size                  = 1
-#   health_check_grace_period = 150
-#   health_check_type         = "EC2"
-#   termination_policies      = ["OldestInstance"]
-#   #vpc_zone_identifier       = [tolist(data.aws_subnets.placement.ids)[1]]
-#   load_balancers            = [aws_lb.artifactory.name]
-#   enabled_metrics           = [
-#     "GroupMinSize",
-#     "GroupMaxSize",
-#     "GroupDesiredCapacity",
-#     "GroupInServiceInstances",
-#     "GroupPendingInstances",
-#     "GroupStandbyInstances",
-#     "GroupTerminatingInstances",
-#     "GroupTotalInstances"
-#   ]
-  
-#   launch_template {
-#     id      = aws_launch_template.artifactory_launch_template.id
-#     version = "$Latest"
-#   }
-  
-#   # tags = {
-#   #   Name = "${var.service}-${var.environment}-asg"
-#   # }
-# }
+resource "aws_autoscaling_group" "artifactory_asg" {
+  name                      = "${var.environment}-${var.service}-asg"
+  max_size                  = var.asg_max_size
+  min_size                  = var.asg_min_size
+  default_cooldown          = var.asg_default_cooldown
+  health_check_grace_period = var.asg_health_check_grace_period
+  health_check_type         = var.asg_health_check_type
+  desired_capacity          = var.asg_desired_capacity
+  load_balancers            = [aws_lb.artifactory.name]
+  vpc_zone_identifier       = [tolist(data.aws_subnets.placement.ids)[1]]
+  termination_policies      = [var.asg_termination_policies]
+  enabled_metrics = [
+    "GroupMinSize",
+    "GroupMaxSize",
+    "GroupDesiredCapacity",
+    "GroupInServiceInstances",
+    "GroupPendingInstances",
+    "GroupStandbyInstances",
+    "GroupTerminatingInstances",
+    "GroupTotalInstances"
+  ]
 
-# resource "aws_autoscaling_policy" "artifactory_asg_policy" {
-#   name                   = "${var.environment}-${var.service}-asg-policy"
-#   policy_type            = "TargetTrackingScaling"
-#   autoscaling_group_name = aws_autoscaling_group.artifactory_asg.id
-  
-#   # CPU Utilization is above 80
-#   target_tracking_configuration {
-#     predefined_metric_specification {
-#       predefined_metric_type = "ASGAverageCPUUtilization"
-#     }
-#     target_value = "80"
-#   }
-  
-#   # tag = {
-#   #   Name = "${var.service}-${var.environment}-asg-policy"
-#   # }
-# }
+  launch_template {
+    id      = aws_launch_template.artifactory_launch_template.id
+    version = "${var.asg_launch_template_version}"
+  }
 
-# resource "aws_autoscaling_attachment" "artifactory_asg_attachment" {
-#   autoscaling_group_name = aws_autoscaling_group.artifactory_asg.id
-#   lb_target_group_arn    = aws_lb_target_group.front_end_8082.arn
-# }
+  tag {
+    key                 = "Account"
+    value               = "${var.account_name}"
+    propagate_at_launch = false
+  }
 
-# resource "aws_autoscaling_schedule" "scale_down" {
-#   scheduled_action_name  = "${var.environment}-${var.service}-scale-down"
-#   min_size               = 1
-#   max_size               = 1
-#   recurrence             = var.asg_scale_down
-#   desired_capacity       = 1
-#   autoscaling_group_name = aws_autoscaling_group.artifactory_asg.name
-# }
+  tag {
+    key                 = "Region"
+    value               = "${var.region}"
+    propagate_at_launch = false
+  }
 
-# resource "aws_autoscaling_schedule" "scale_up" {
-#   scheduled_action_name  = "${var.environment}-${var.service}-scale-up"
-#   min_size               = 1
-#   max_size               = 1
-#   recurrence             = var.asg_scale_up
-#   desired_capacity       = 1
-#   autoscaling_group_name = aws_autoscaling_group.artifactory_asg.name
-# }
+  tag {
+    key                 = "Name"
+    value               = "${var.service}-${var.environment}"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Service"
+    value               = "${var.service}"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Environment"
+    value               = "${var.environment}"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Managed By Terraform"
+    value               = "true"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "artifactory_asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.artifactory_asg.id
+  lb_target_group_arn    = aws_lb_target_group.front_end_8082.arn
+}
+
+resource "aws_autoscaling_schedule" "scale_down" {
+  desired_capacity       = var.asg_scale_down_desired_capacity
+  min_size               = var.asg_scale_down_min_size
+  max_size               = var.asg_scale_down_max_size
+  recurrence             = var.asg_scale_down_recurrence
+  scheduled_action_name  = "${var.environment}-${var.service}-scale-down"
+  autoscaling_group_name = aws_autoscaling_group.artifactory_asg.name
+}
+
+resource "aws_autoscaling_schedule" "scale_up" {
+  desired_capacity       = var.asg_desired_capacity
+  min_size               = var.asg_min_size
+  max_size               = var.asg_max_size
+  recurrence             = var.asg_scale_up_recurrence
+  scheduled_action_name  = "${var.environment}-${var.service}-scale-up"
+  autoscaling_group_name = aws_autoscaling_group.artifactory_asg.name
+}
--- groups/instance/launch_template.tf
+++ groups/instance/launch_template.tf
@@ -1,24 +1,23 @@
 resource "aws_launch_template" "artifactory_launch_template" {
-  name                   = "${var.service}-${var.environment}-launch-template"
-  image_id               = data.aws_ami.artifactory_ami.id
-  instance_type          = var.default_instance_type
-  key_name               = local.ssh_keyname
-  user_data              = data.cloudinit_config.artifactory.rendered
-  #vpc_security_group_ids = [aws_security_group.instance_security_group.id]
-  
+  name          = "${var.service}-${var.environment}-launch-template"
+  image_id      = data.aws_ami.artifactory_ami.id
+  instance_type = var.default_instance_type
+  key_name      = local.ssh_keyname
+  user_data     = data.cloudinit_config.artifactory.rendered
+
   lifecycle {
     create_before_destroy = true
   }
-  
+
   network_interfaces {
     security_groups = [aws_security_group.instance_security_group.id]
     subnet_id       = tolist(data.aws_subnets.placement.ids)[1]
   }
-  
+
   iam_instance_profile {
     name = aws_iam_instance_profile.artifactory_instance_profile.name
   }
-  
+
   block_device_mappings {
     device_name = var.block_device_name
     ebs {
@@ -31,7 +30,7 @@ resource "aws_launch_template" "artifactory_launch_template" {
       volume_type           = var.ebs_root_volume_type
     }
   }
-  
+
   tags = {
     Name = "${var.service}-${var.environment}-asg-launch-template"
   }
