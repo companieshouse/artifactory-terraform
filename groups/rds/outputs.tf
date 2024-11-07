f595cfbcf9dead3c730b109b5a9c7d2d9d9de5d3
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2024-11-07 12:47:10 +0000

    Cleanup and add outputs

--- groups/rds/outputs.tf
+++ groups/rds/outputs.tf
@@ -1,7 +1,19 @@
-output vpc_ids {
-  value = data.aws_vpc.placement.id
+output "vpc_id" {
+  description = "Returns the id of the VPC used during deployment"
+  value       = data.aws_vpc.placement.id
 }
 
-output subnet_ids {
-  value = local.placement_subnet_ids
+output "subnet_ids" {
+  description = "Returns a list of the subnet ids used during deployment"
+  value       = local.placement_subnet_ids
+}
+
+output "security_group_id" {
+  description = "Returns the id of the RDS security group"
+  value       = aws_security_group.rds.id
+}
+
+output "rds_route53_name" {
+  description = "Returns the Route53 name, or FQDN, for the RDS instance"
+  value       = aws_route53_entry.db.name
 }
