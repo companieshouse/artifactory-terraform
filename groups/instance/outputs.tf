7b30144317257efd17e56810950000332a408fc6
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2024-11-07 13:00:04 +0000

    Update outputs

--- groups/instance/outputs.tf
+++ groups/instance/outputs.tf
@@ -1,3 +1,24 @@
-output dns_hostname {
-  value = aws_route53_record.instance.fqdn
+output "alb_dns_name" {
+  description = "Returns the AWS DNS name for the ALB"
+  value       = aws_lb.artifactory.dns_name
+}
+
+output "alb_name" {
+  description = "Returns the name of the deployed ALB"
+  value       = aws_lb.artifactory.name
+}
+
+output "alb_security_group_id" {
+  description = "Returns the id of the security group attached to the ALB"
+  value       = aws_security_group.alb.id
+}
+
+output "asg_name" {
+  description = "Returns the name of the deployed ASG"
+  value       = aws_autoscaling_group.artifactory_asg.name
+}
+
+output "asg_security_group_id" {
+  description = "Returns the id of the security group attached to ASG EC2 instances"
+  value       = aws_security_group.ec2.id
 }
