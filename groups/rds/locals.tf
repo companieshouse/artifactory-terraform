e0e156c1047ace34bc71fea652c2c24632990643
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2024-12-09 09:36:06 +0000

    Add RDS monitoring role

--- groups/rds/locals.tf
+++ groups/rds/locals.tf
@@ -15,6 +15,8 @@ locals {
   placement_subnet_pattern = local.secrets.placement_subnet_pattern
   placement_vpc_pattern    = local.secrets.placement_vpc_pattern
 
+  current_account_id = data.aws_caller_identity.current.account_id
+
   dns_zone_name = local.secrets.dns_zone_name
 
   db_username = local.secrets.db_username
@@ -34,4 +36,6 @@ locals {
 
   db_storage_iops       = var.db_storage_gb < local.db_storage_threshold ? local.db_storage_map.small.iops : local.db_storage_map.large.iops
   db_storage_throughput = var.db_storage_gb < local.db_storage_threshold ? local.db_storage_map.small.throughput : local.db_storage_map.large.throughput
+
+  rds_cloudwatch_logs_arn_prefix = "arn:aws:logs:${var.region}:${local.current_account_id}"
 }
